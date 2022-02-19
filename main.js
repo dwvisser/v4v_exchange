a3687fe2bd83c030901cce65e68cec22ce9a0cd7
Dale Visser <dale.visser@live.com>
2022-02-19 09:03:48 -0500

    Default to $1/hour, allow user to make persistent $/hour setting.

--- main.js
+++ main.js
@@ -17,15 +17,38 @@ function showDuration(last_updated) {
 }
 
 let timeoutID = 0;
+let usd_per_btc = 40000;
 
 function updateAgo(last_updated) {
-    document.querySelector('#date').innerHTML = showDuration(last_updated);
+    document.getElementById('date').innerHTML = showDuration(last_updated);
     timeoutID = setTimeout(updateAgo, 1000, last_updated);
+}
+
+function modifyUsdRate() {
+    localStorage.setItem('usd_per_hr', document.getElementById('usd_per_hr').value);
+    calculateSatRate();
+}
+
+function calculateSatRate() {
+    let sat_per_hour = getUsdPerHr() / usd_per_btc * 100000000;
+    document.querySelector('#sat-rate').innerHTML = Math.round(sat_per_hour / 60);
 }
 
 function main() {
     doFetch();
-    document.querySelector('#update').addEventListener("click", doFetch);
+    document.getElementById('update').addEventListener('click', doFetch);
+    document.getElementById('usd_per_hr').addEventListener('change', modifyUsdRate);
+}
+
+
+function getUsdPerHr() {
+    let rval = localStorage.getItem('usd_per_hr');
+    if (rval == null) {
+        localStorage.setItem('usd_per_hr', '1.00');
+        rval = localStorage.getItem('usd_per_hr');
+    }
+    document.getElementById('usd_per_hr').value = rval;
+    return parseFloat(rval);
 }
 
 async function doFetch() {
@@ -37,9 +60,8 @@ async function doFetch() {
         clearTimeout(timeoutID);
         updateAgo(last_updated);
         document.querySelector('#usd-per-btc').innerHTML = jsonValue.bpi.USD.rate;
-        let usd_per_btc = jsonValue.bpi.USD.rate_float;
-        let sat_per_hour = 0.25 / usd_per_btc * 100000000;
-        document.querySelector('#sat-rate').innerHTML = Math.round(sat_per_hour / 60);
+        usd_per_btc = jsonValue.bpi.USD.rate_float;
+        calculateSatRate();
     } else {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
